3f36396b5892c575a9c41ffcbe046385414fcfee
A Shakti Rao <ashaktirao@gmail.com>
2026-04-01 11:07:11 +0530

    Sign up ui completed

--- Frontend/src/components/UserAuthPage.jsx
+++ Frontend/src/components/UserAuthPage.jsx
@@ -2,28 +2,25 @@ import { useState } from "react";
 import googleIcon from "../assets/google.png";
 import facebookIcon from "../assets/facebook.png";
 import githubIcon from "../assets/github.png";
+import imgUpload from "../assets/upload-img.png";
 
 function UserAuthPage() {
-  const [isSignIn, setIsSignIn] = useState(true);
+  const [isSignIn, setIsSignIn] = useState(false);
 
   return (
     <div className={`h-screen w-screen flex justify-center items-center`}>
-      <div className="w-[40%] h-[80%] rounded-2xl overflow-hidden">
-        {isSignIn ? (
-          <SignIn setIsSignIn={setIsSignIn} />
-        ) : (
-          <SignUp setIsSignIn={setIsSignIn} />
-        )}
-      </div>
+      {isSignIn ? (
+        <SignIn setIsSignIn={setIsSignIn} />
+      ) : (
+        <SignUp setIsSignIn={setIsSignIn} />
+      )}
     </div>
   );
 }
 
 function SignIn({ setIsSignIn }) {
   return (
-    <div
-      className={`transition-all ease-in-out duration-700 h-full flex flex-col justify-center space-y-10 p-10 bg-black/20 text-sm`}
-    >
+    <div className="transition-all rounded-lg ease-in-out duration-700 flex flex-col justify-center space-y-10 p-10 bg-black/20 text-sm">
       <div className="space-y-2">
         <h1 className="text-4xl font-bold">Sign in!</h1>
         <p className="text-sm">Enter to go unlimited access to your data</p>
@@ -112,45 +109,82 @@ function SignIn({ setIsSignIn }) {
 
 function SignUp({ setIsSignIn }) {
   return (
-    <div
-      className={`transition-all ease-in-out duration-700 h-full flex flex-col justify-center space-y-10 p-10 bg-black/20 text-sm`}
-    >
+    <div className="rounded-lg w-[45%] transition-all ease-in-out duration-700 flex flex-col justify-center space-y-8 p-10 bg-black/20 text-sm">
       <div className="space-y-2">
         <h1 className="text-4xl font-bold">Sign up!</h1>
-        <p className="text-sm">Enter to details to register</p>
+        <p className="text-sm">Enter to details to create an account</p>
       </div>
 
-      <form className="flex flex-col gap-3">
-        <Input name={"Fullname"} type={"text"} msg={"Enter your full name"} />
-        <Input name={"email"} type={"text"} msg={"Enter your email address"} />
-        <Input name={"Username"} type={"text"} msg={"set your username"} />
-        <Input
-          name={"Password"}
-          type={"password"}
-          msg={"set a strong password"}
-        />
-
-        <div className="flex text-xs justify-between items-center">
-          <div>
-            <input
-              className="w-2.5 mr-0.5 align-middle cursor-grab active:cursor-grabbing"
-              type="checkbox"
-              name="remember"
-              id="remember"
-            />
+      <form className="flex flex-col gap-6">
+        <div className="flex gap-5">
+          <div className="border-r-3 flex flex-col justify-center items-center border-myGraySoft w-1/3">
             <label
-              htmlFor="remember"
-              className="cursor-grab active:cursor-grabbing"
+              htmlFor="defaultPicture"
+              className="flex flex-col h-[50%] items-center cursor-pointer gap-1.5"
             >
-              Remember me
+              <div className=" outline-2 hover:bg-myGraySoft/50 outline-fuchsia-500 size-25 rounded-full flex justify-center items-center">
+                <img className="w-10 object-contain" src={imgUpload} />
+              </div>
+              <span className="border-2 bg-myGraySoft hover:bg-myGraySoft/50 border-fuchsia-500 pr-4 pl-4 pt-1 pb-1 rounded-lg text-xs">
+                Upload Image
+              </span>
             </label>
+            <input
+              type="file"
+              name="defaultPicture"
+              id="defaultPicture"
+              accept="image/*"
+              hidden
+            />
+            <span className="text-xs">Already have an account!!</span>
+            <button
+              className="text-fuchsia-500 hover:text-fuchsia-800 cursor-pointer text-xs underline font-semibold"
+              type="text"
+              onClick={() => setIsSignIn(true)}
+            >
+              Sign In
+            </button>
           </div>
-          <div className="cursor-grab active:cursor-grabbing">
-            <span>Forgot your password</span>
-            <span className="text-[8px] text-red-500 align-super">★</span>
+          <div className="w-2/3 space-y-2">
+            <Input
+              name={"Fullname"}
+              type={"text"}
+              msg={"Enter your full name"}
+            />
+            <Input
+              name={"email"}
+              type={"text"}
+              msg={"Enter your email address"}
+            />
+            <Input name={"Username"} type={"text"} msg={"set your username"} />
+            <Input
+              name={"Password"}
+              type={"password"}
+              msg={"set a strong password"}
+            />
+
+            <div className="flex text-xs justify-between items-center">
+              <div>
+                <input
+                  className="w-2.5 mr-0.5 align-middle cursor-grab active:cursor-grabbing"
+                  type="checkbox"
+                  name="remember"
+                  id="remember"
+                />
+                <label
+                  htmlFor="remember"
+                  className="cursor-grab active:cursor-grabbing"
+                >
+                  Remember me
+                </label>
+              </div>
+              <div className="cursor-grab active:cursor-grabbing">
+                <span>Forgot your password</span>
+                <span className="text-[8px] text-red-500 align-super">★</span>
+              </div>
+            </div>
           </div>
         </div>
-
         <button
           className="bg-fuchsia-500 hover:bg-fuchsia-600 text-sm pt-2 pb-2 cursor-pointer rounded-lg"
           type="submit"
