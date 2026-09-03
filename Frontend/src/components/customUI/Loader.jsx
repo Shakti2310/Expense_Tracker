0fe8e5dc6d14fb20f6d2ae1cd6987dcad549993e
A Shiva Rao <shivarao6102@gmail.com>
2026-09-03 15:17:43 +0530

    fix: adjust Loader component to occupy full height and width

--- Frontend/src/components/customUI/Loader.jsx
+++ Frontend/src/components/customUI/Loader.jsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button.jsx";
 
 function Loader({ text = "Loading...", ...props }) {
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className="flex flex-col w-full min-h-screen items-center justify-center gap-4">
       <Button variant="outline" disabled size="sm">
         <Spinner data-icon="inline-start" />
         {text}
