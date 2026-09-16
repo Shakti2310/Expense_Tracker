97046a7321cea2c541b1418a6ce0d936e9d3222f
A Shiva Rao <shivarao6102@gmail.com>
2026-09-16 11:31:41 +0530

    refactor: remove LoggingContext and related state management from AppLayout

--- Frontend/src/contexts/LoggingContext.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const LoggingContext = createContext(false);
--- Frontend/src/layouts/AppLayout.jsx
+++ Frontend/src/layouts/AppLayout.jsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
 import { Outlet } from "react-router";
-import { LoggingContext } from "../contexts/LoggingContext.jsx";
 import AppSidebar from "../components/layout/app/AppSidebar.jsx";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import AppNavbar from "@/components/layout/app/AppNavbar.jsx";
 
 function AppLayout() {
-  const [isLogged, setIsLogged] = useState(true);
   return (
-    <div className="min-h-dvh font-nunito flex">
+    <div className="min-h-dvh font-nunito flex ">
       <SidebarProvider>
-        <LoggingContext.Provider value={setIsLogged}>
           <AppSidebar />
-          <div className="w-full bg-gray-100 dark:bg-black">
+          <div className="w-full bg-gray-00 dark:bg-black">
             <AppNavbar />
             <Outlet />
           </div>
-        </LoggingContext.Provider>
       </SidebarProvider>
     </div>
   );
