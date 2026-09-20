1e10a6c46e7fad2ed43385cba3ef2b193296e62c
A Shiva Rao <shivarao6102@gmail.com>
2026-09-20 16:29:22 +0530

    refactor: simplify resendOtp by removing email verification token logic

--- Backend/src/controllers/otp.controller.js
+++ Backend/src/controllers/otp.controller.js
@@ -1,19 +1,16 @@
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
-import { cookieOptions1d } from "../constants.js";
-import { initiateEmailVerification } from "../services/verification.service.js";
+import sendNewOtp from "../services/otp.service.js";
 
 const resendOtp = asyncHandler(async (req, res) => {
   // Check if already verified
   if (req.user.isVerified) throw new ApiError(400, "User is already verified");
 
-  const verificationToken = await initiateEmailVerification(req.user);
+  const otp = sendNewOtp(req.user);
+  if (!otp) throw new ApiError(500, "Otp sending failed");
 
-  return res
-    .status(200)
-    .cookie("verificationToken", verificationToken, cookieOptions1d)
-    .json(new ApiResponse(200, "OTP resent to your email"));
+  return res.status(200).json(new ApiResponse(200, "OTP resent to your email"));
 });
 
 export { resendOtp };
